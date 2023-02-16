a16eb8f806ee0ffb9321277fae1248d68050855a
MakAnton338 <makanton95@gmail.com>
2023-02-16 19:09:26 +0200

    Just_main

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-def parse(query: str) -> dict:
-    return {}
+from urllib.parse import parse_qsl, urlparse
 
+def parse(url: str) -> dict:
+    parsed_url = urlparse(url)
+    q = dict(parse_qsl(parsed_url.query))
+    return q
 
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-
+    assert parse('http://example.com/?name=John') == {'name': 'John'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&sex=male') == {'name': 'ferret', 'color': 'purple', 'sex': 'male'}
+    assert parse('https://example.com/path/to/page?name==ferret&color=purple&') == {'name': '=ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple!') == {'name': 'ferret', 'color': 'purple!'}
+    assert parse('https://example.com/path/to/page?name==ferret&') == {'name': '=ferret'}
+    assert parse('http://example.com/path/to/page?name=John&123==pp') == {'name': 'John', '123': '=pp'}
+    assert parse('https://example.com/path/to/page?name=ferret&?=purple') == {'name': 'ferret', '?': 'purple'}
+    assert parse('https://example.com/path/to/page?name=&color=&') == {}
+    assert parse('https://example.com/path/to/page?name=ferret&&&&&&&&&?=purple') == {'name': 'ferret', '?': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&?color=purple') == {'name': 'ferret', '?color': 'purple'}
+    assert parse('?name=ferret&color=purple?') == {'name': 'ferret', 'color': 'purple?'}
 
 def parse_cookie(query: str) -> dict:
     return {}
